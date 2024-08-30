f2c596533d2b4f7575b71d16d1f670f4e548a1af
brandonhach <specialcambo@gmail.com>
2024-08-30 12:51:04 -0400

    removed

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint', 'prisma'],
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prisma/recommended'],
-	rules: {
-		// You can add custom rules here
-	},
-};
