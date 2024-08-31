6929c67d44640d6d7634a99deaa91c2d65ec0c65
brandonhach <specialcambo@gmail.com>
2024-08-31 16:21:26 -0400

    initial commit

--- server/src/controllers/dashboardController.ts
+++ server/src/controllers/dashboardController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { db } from '../service/prisma.service';
 
-
+// Getting 5 different data sources
 export const getDashboardMetrics = async (req: Request, res: Response): Promise<void> => {
 	try {
 		const popularProducts = await db.products.findMany({
@@ -11,7 +11,42 @@ export const getDashboardMetrics = async (req: Request, res: Response): Promise<
 			},
 		});
 
-		const salesSummary = await db.
+		const salesSummary = await db.salesSummary.findMany({
+			take: 5,
+			orderBy: {
+				date: 'desc',
+			},
+		});
+		const purchaseSummary = await db.purchaseSummary.findMany({
+			take: 5,
+			orderBy: {
+				date: 'desc',
+			},
+		});
+		const expenseSummary = await db.expenseSummary.findMany({
+			take: 5,
+			orderBy: {
+				date: 'desc',
+			},
+		});
+		const expenseByCategorySummaryRaw = await db.expenseByCategory.findMany({
+			take: 5,
+			orderBy: {
+				date: 'desc',
+			},
+		});
+		const expenseByCategorySummary = expenseByCategorySummaryRaw.map((item) => ({
+			...item,
+			amount: item.amount.toString(),
+		}));
+
+		res.json({
+			popularProducts,
+			salesSummary,
+			purchaseSummary,
+			expenseSummary,
+			expenseByCategorySummary,
+		});
 	} catch (error) {
 		res.status(500).json({ message: 'Error retrieving dashboard metrics' });
 	}
